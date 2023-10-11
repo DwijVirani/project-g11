3d9d77fe9981ac4ea41c5953d420f0841d9fde7c
Dwij Virani <71424352+DwijVirani@users.noreply.github.com>
2023-10-11 01:24:03 -0400

    Fixed: Wrong destination for photo upload

--- app/middlewares/fileUpload.js
+++ app/middlewares/fileUpload.js
@@ -2,7 +2,7 @@ const path = require('path');
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, './public/photos'),
+  destination: path.join(__dirname, '../public/photos'),
   filename(req, file, cb) {
     cb(null, `${Date.now()}${path.extname(file.originalname)}`);
   },
